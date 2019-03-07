67744eed76280a54096077c1d5a068ce01ec6737
Hubert Stasiuk <hstasiuk@coreservices.pl>
2019-03-07 19:09:54 +0100

    Role POJO Impl

---
+++ Tasteland-Server/src/main/java/com/tasteland/app/Tasteland/model/Role.java
@@ -0,0 +1,20 @@
+package com.tasteland.app.Tasteland.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "role")
+@Data
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+
+
+}
