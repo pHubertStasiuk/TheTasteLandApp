fee15c5d106055fb8cdbf94e011fd1a84c2efa50
Hubert Stasiuk <hstasiuk@coreservices.pl>
2019-03-07 19:13:06 +0100

    UserValidator helper class impl

---
+++ Tasteland-Server/src/main/java/com/tasteland/app/Tasteland/model/UserValidator.java
@@ -0,0 +1,41 @@
+package com.tasteland.app.Tasteland.model;
+
+import com.tasteland.app.Tasteland.utils.FieldMatch;
+import com.tasteland.app.Tasteland.utils.ValidEmail;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@FieldMatch.List({
+        @FieldMatch(first = "password", second = "matchingPassword", message = "The password fields must match")
+})
+@Data
+public class UserValidator {
+
+    @NotNull(message = "is required")
+    @Size(min = 1, message = "is required")
+    private String userName;
+
+    @NotNull(message = "is required")
+    @Size(min = 1, message = "is required")
+    private String password;
+
+    @NotNull(message = "is required")
+    @Size(min = 1, message = "is required")
+    private String matchingPassword;
+
+    @NotNull(message = "is required")
+    @Size(min = 1, message = "is required")
+    private String firstName;
+
+    @NotNull(message = "is required")
+    @Size(min = 1, message = "is required")
+    private String lastName;
+
+    @ValidEmail
+    @NotNull(message = "is required")
+    @Size(min = 1, message = "is required")
+    private String email;
+
+}
