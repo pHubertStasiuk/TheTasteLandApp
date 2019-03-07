3108412acbef79fc7781c2853713c9c06982427a
Hubert Stasiuk <hstasiuk@coreservices.pl>
2019-03-07 19:04:59 +0100

    Configuration class impl for database connectivity

---
+++ Tasteland-Server/src/main/java/com/tasteland/app/Tasteland/config/TastelandConfig.java
@@ -0,0 +1,99 @@
+package com.tasteland.app.Tasteland.config;
+
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan(basePackages = "com.tasteland.app")
+@PropertySource("classpath:persistence-mysql.properties")
+public class TastelandConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public DataSource dataSource() {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+
+        // set the jdbc driver
+        try {
+            dataSource.setDriverClass(env.getProperty("jdbc.driver"));
+        } catch (PropertyVetoException exception) {
+            throw new RuntimeException();
+        }
+
+        // set database connection props
+        dataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+        dataSource.setUser(env.getProperty("jdbc.user"));
+        dataSource.setPassword(env.getProperty("jdbc.password"));
+
+        // set connection pool props
+        dataSource.setInitialPoolSize(
+                getIntProperty("connection.pool.initialPoolSize"));
+
+        dataSource.setMinPoolSize(
+                getIntProperty("connection.pool.minPoolSize"));
+
+        dataSource.setMaxPoolSize(
+                getIntProperty("connection.pool.maxPoolSize"));
+
+        dataSource.setMaxIdleTime(
+                getIntProperty("connection.pool.maxIdleTime"));
+
+        dataSource.setNumHelperThreads(6);
+        dataSource.setMaxAdministrativeTaskTime(80);
+
+        return dataSource;
+
+    }
+    private int getIntProperty(String propName) {
+        String propVal = env.getProperty(propName);
+        int intPropVal = Integer.parseInt(propVal);
+        return intPropVal;
+    }
+
+    private Properties getHibernateProperties() {
+        Properties props = new Properties();
+        props.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        props.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+        props.setProperty("hibernate.format_sql", env.getProperty("hibernate.format_sql"));
+        return props;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan(env.getProperty("hibernate.packagesToScan"));
+        sessionFactory.setHibernateProperties(getHibernateProperties());
+
+
+        return sessionFactory;
+    }
+
+    @Bean
+    @Autowired
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager txManager = new HibernateTransactionManager();
+        txManager.setSessionFactory(sessionFactory);
+        return txManager;
+    }
+
+
+}
