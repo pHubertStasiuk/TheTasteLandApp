9c15560178868a853d37888c11460c4decb65a6e
Hubert Stasiuk <hstasiuk@coreservices.pl>
2019-03-07 19:08:59 +0100

    User POJO impl

---
+++ Tasteland-Server/src/main/java/com/tasteland/app/Tasteland/model/User.java
@@ -0,0 +1,39 @@
+package com.tasteland.app.Tasteland.model;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Data
+@RequiredArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Collection<Role> roles;
+}
