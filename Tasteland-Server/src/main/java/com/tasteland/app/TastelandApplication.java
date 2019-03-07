bbc4156bb3d687b8f4ead56937947a3a1a9ae81d
Hubert Stasiuk <hstasiuk@coreservices.pl>
2019-03-07 19:12:17 +0100

    Main springboot app class

---
+++ Tasteland-Server/src/main/java/com/tasteland/app/TastelandApplication.java
@@ -0,0 +1,13 @@
+package com.tasteland.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastelandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TastelandApplication.class, args);
+	}
+
+}
