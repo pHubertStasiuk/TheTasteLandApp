463a23dd88fd2c3f085bc87eacecd3c47f50ef92
Hubert Stasiuk <hstasiuk@coreservices.pl>
2019-03-09 17:13:47 +0100

    App component && modules && routings

--- Tasteland-Angular/src/app/app.module.ts
+++ Tasteland-Angular/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { ComponentsModule } from './components/components.module';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminLayoutComponent
   ],
   imports: [
+    BrowserAnimationsModule,
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    HttpClientModule,
+    RouterModule,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
