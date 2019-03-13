33c950b6c248a63538db08b25fa769ed64763ac4
Hubert Stasiuk <hstasiuk@coreservices.pl>
2019-03-13 20:53:23 +0100

    User and role POJOs

--- Tasteland-Server/src/main/java/com/tasteland/app/Tasteland/model/Role.java
+++ Tasteland-Server/src/main/java/com/tasteland/app/Tasteland/model/Role.java
@@ -1,13 +1,14 @@
 package com.tasteland.app.Tasteland.model;
 
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "role")
 @Data
-public class Role {
+public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,5 +18,8 @@ public class Role {
     private String name;
 
 
-
+    @Override
+    public String getAuthority() {
+        return null;
+    }
 }
--- Tasteland-Server/src/main/java/com/tasteland/app/Tasteland/model/User.java
+++ Tasteland-Server/src/main/java/com/tasteland/app/Tasteland/model/User.java
@@ -4,7 +4,9 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
-import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Data
@@ -13,7 +15,7 @@ import java.util.Collection;
         @NamedQuery(name = "getUserByUsername", query = "SELECT u FROM User u WHERE u.username=:uName"),
         @NamedQuery(name = "getAllUsers", query = "SELECT u FROM User u"),
         @NamedQuery(name = "getUserById", query = "SELECT u FROM User u WHERE u.id=:uId"),
-        @NamedQuery(name = "deleteUserById", query = "DELETE u FROM User u WHERE u.id=:uId")
+        @NamedQuery(name = "deleteUserById", query = "DELETE FROM User u WHERE u.id=:uId")
 })
 public class User {
 
@@ -37,9 +39,15 @@ public class User {
     @Column(name = "email")
     private String email;
 
+    @Column(name = "last_password_reset")
+    private Date lastPasswordResetDate;
+
+    @Column(name = "enabled")
+    private Boolean enabled;
+
     @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinTable(name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Collection<Role> roles;
+    private Set<Role> roles;
 }
